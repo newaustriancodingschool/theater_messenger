449dcd56a08ef1b3e86e914169c457f997e983ea
alirezaasghari <alireza.asghari87@gmail.com>
2018-05-07 17:15:13 +0200

    Add project structure

---
+++ src/main/java/at/refugeescode/theater_messenger/TheaterMessengerApplication.java
@@ -0,0 +1,12 @@
+package at.refugeescode.theater_messenger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterMessengerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheaterMessengerApplication.class, args);
+	}
+}
---
+++ src/test/java/at/refugeescode/theater_messenger/TheaterMessengerApplicationTests.java
@@ -0,0 +1,16 @@
+package at.refugeescode.theater_messenger;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TheaterMessengerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
