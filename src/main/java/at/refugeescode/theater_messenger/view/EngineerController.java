fa0fd15bbe84770fd569831e9b863f33bc4d8777
alirezaasghari <alireza.asghari87@gmail.com>
2018-05-12 16:25:09 +0200

    Edit and delete repositories

--- src/main/java/at/refugeescode/theater_messenger/view/EngineerController.java
+++ src/main/java/at/refugeescode/theater_messenger/view/EngineerController.java
@@ -3,10 +3,12 @@ package at.refugeescode.theater_messenger.view;
 import at.refugeescode.theater_messenger.controller.ProjectController;
 import at.refugeescode.theater_messenger.controller.SoundEngineer;
 import at.refugeescode.theater_messenger.persistence.model.Project;
+import at.refugeescode.theater_messenger.persistence.repository.ProjectRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/engineer")
@@ -16,6 +18,12 @@ public class EngineerController {
 
     public EngineerController(ProjectController projectController) {
         this.projectController = projectController;
+    }
+
+    // engineer is the name of HTML file
+    @GetMapping
+    String page() {
+        return "engineer";
     }
 
     // Form object
@@ -36,17 +44,37 @@ public class EngineerController {
         return projectController.showAllProjects();
     }
 
-    @GetMapping("/editproject/{id}")
-    String editProject(@RequestParam Long id) {
-        projectController.findProject(id);
-        return "redirect:/editproject";
-    }
 
 
-    // engineer is the name of HTML file
-    @GetMapping
-    String page() {
-        return "engineer";
-    }
+
+//    @PostMapping(value = "/editproject",params = "projectId")
+//    String editProject(@RequestParam Long projectId) {
+//        projectController.findProject(projectId);
+//        System.out.println(projectId);
+//        return "redirect:/editproject";
+//    }
+
+//    Project project = new Project();
+//
+//    @ModelAttribute("p")
+//    Project editParticipant(){
+//        return  project;
+//    }
+
+//    @PostMapping("update")
+//    String goedit(@RequestParam String id ){
+//
+//        Optional<Project> oldProject = projectRepository.findAll().stream()
+//                .filter(project -> project.getId().toString().equalsIgnoreCase(id))
+//                .findFirst();
+//        project =oldProject.get();
+//        return "redirect:/edit";
+//    }
+//
+//    @GetMapping("/edit")
+//    String page6(){
+//        return "edit";
+//    }
+
 
 }
